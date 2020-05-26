f30a898deda9c6bc8c82c9a7c467bb6ea592aa83
Silver Savi <silver.savi@gmail.com>
2020-05-26 12:54:14 +0300

    Updates

---
+++ precache-manifest.3b15eb38c24ffa5f53bb21f72b5409c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5f9351e26add3847456315b2ce1a09",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e40eec2cc6071f8bba6a",
+    "url": "/monster-rolodex/static/css/main.ef91681d.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40eec2cc6071f8bba6a",
+    "url": "/monster-rolodex/static/js/main.01dd4690.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
